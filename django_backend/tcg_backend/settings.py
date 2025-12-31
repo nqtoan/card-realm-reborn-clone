15821f134234c8be6f653544d8dd5f05b99375a0
nqtoan <toan19001@gmail.com>
2025-12-31 14:43:48 +0700

    feat: update a Django backend deployed on Render

--- django_backend/tcg_backend/settings.py
+++ django_backend/tcg_backend/settings.py
@@ -2,6 +2,7 @@
 import os
 from pathlib import Path
 from decouple import config
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -58,10 +59,11 @@ TEMPLATES = [
 ]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
 
 REST_FRAMEWORK = {
